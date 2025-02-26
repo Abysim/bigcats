4d1fc00228e0e8b52f5b402f81fe1dcf0447b610
Abysim <abysim@whitelion.me>
2025-02-26 04:26:22 +0200

    https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use Filament\Support\Colors\Color;
 use Filament\Support\Facades\FilamentColor;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -22,6 +23,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        URL::forceHttps($this->app->isProduction());
+
         FilamentColor::register([
             'danger' => Color::Red,
             'gray' => Color::Blue,
