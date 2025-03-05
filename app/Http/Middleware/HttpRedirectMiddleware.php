2281d6b0d5ad63426ea677a28278663d14766b20
Abysim <abysim@whitelion.me>
2025-03-05 20:06:30 +0200

    Redirect all domains to the main

--- app/Http/Middleware/HttpRedirectMiddleware.php
+++ app/Http/Middleware/HttpRedirectMiddleware.php
@@ -5,13 +5,17 @@ namespace App\Http\Middleware;
 use Closure;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\App;
+use Illuminate\Support\Str;
 
 class HttpRedirectMiddleware
 {
     public function handle(Request $request, Closure $next)
     {
-        if (!$request->secure() && App::isProduction()) {
-            return redirect()->secure($request->getRequestUri(), 301);
+        if (
+            (!$request->secure() || $request->header('host') !== Str::after(config('app.url'), '://'))
+            && App::isProduction()
+        ) {
+            return redirect(config('app.url') . $request->getRequestUri(), 301);
         }
 
         return $next($request);
